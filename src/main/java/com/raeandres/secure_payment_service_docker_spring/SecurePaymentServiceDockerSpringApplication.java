91f8d1d6052e1e474f22daadc6121f9141c6c5bc
Rae-An Andres <raeandres@users.noreply.github.com>
2025-09-28 02:15:15 +0800

    init commit

---
+++ src/main/java/com/raeandres/secure_payment_service_docker_spring/SecurePaymentServiceDockerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.raeandres.secure_payment_service_docker_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurePaymentServiceDockerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurePaymentServiceDockerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raeandres/secure_payment_service_docker_spring/SecurePaymentServiceDockerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raeandres.secure_payment_service_docker_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurePaymentServiceDockerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
